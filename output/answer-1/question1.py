63c714e5ba91545df45e4f32bba1cbca418bfae7
DaksheshShah <55339746+DaksheshShah@users.noreply.github.com>
2021-10-21 13:09:57 +0530

    Delete question1.py

--- output/answer-1/question1.py
+++
@@ -1,8 +0,0 @@
-
-import pandas as pd
-df = pd.read_csv('input/question-1/main.csv')
-filter = df["Year"]%10 == 0
-df.where(filter, inplace = True)
-df = df.dropna()
-df.to_csv('output/answer-1/answer1.csv',index=False)
-print(df)
